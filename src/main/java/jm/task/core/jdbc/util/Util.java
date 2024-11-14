844fb5db804b3f8c700bd056b8c48c642393c6f3
Дарья <ms@dkorsakova.ru>
2024-11-14 22:51:14 +0300

    util getConnection finish

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.*;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/sakila";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Dashok_191";
+    static Connection connection;
+
+    {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
