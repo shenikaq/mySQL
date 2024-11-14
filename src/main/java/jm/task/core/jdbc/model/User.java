dac754ac2d7dd110c05aa8ab3e6cad6225255928
Дарья <ms@dkorsakova.ru>
2024-11-14 23:15:37 +0300

    user @override toString() finish

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -28,6 +28,11 @@ public class User {
         this.age = age;
     }
 
+    @Override
+    public String toString() {
+        return "name: " + name + ", lastName: " + lastName + ", age: " + age;
+    }
+
     public Long getId() {
         return id;
     }
